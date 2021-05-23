c69f21b24569bb2f114a0714c581f282052cc2bd
Md.Rafsanjani <rafsan.rifat255@gmail.com>
2021-05-23 16:52:45 +0600

    data loaded from json file in html file

--- django-env/bookstore/books/views.py
+++ django-env/bookstore/books/views.py
@@ -1,9 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import json
+from pathlib import Path
+
+booksData = open("books.json").read()
+data = json.loads(booksData)
+
 
 
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello Books App')  eita savabik khetre. r templates er somoy return render use korte hobe, eita shortcut.
-    context = {'name': 'Rafsan'}
+    context = {'books': data}
     return render(request, 'books/index.html', context)
