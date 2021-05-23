c0da87ab8ef6d7eb36f6dd73cd271202cf78bc5f
Md.Rafsanjani <rafsan.rifat255@gmail.com>
2021-05-23 14:23:54 +0600

    json file added

--- django-env/bookstore/books/views.py
+++ django-env/bookstore/books/views.py
@@ -5,4 +5,5 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello Books App')  eita savabik khetre. r templates er somoy return render use korte hobe, eita shortcut.
-    return render(request, 'index.html', )
+    context = {'name': 'Rafsan'}
+    return render(request, 'books/index.html', context)
