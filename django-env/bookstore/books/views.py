4992ab99b5b99267ed186cc4745d78ff5e42e5df
Md.Rafsanjani <rafsan.rifat255@gmail.com>
2021-05-23 12:20:20 +0600

    templates folder created

--- django-env/bookstore/books/views.py
+++ django-env/bookstore/books/views.py
@@ -4,4 +4,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello Books App')
+    # return HttpResponse('Hello Books App')  eita savabik khetre. r templates er somoy return render use korte hobe, eita shortcut.
+    return render(request, 'index.html', )
