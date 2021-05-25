5b1f6acc63f87e703a0a2eb5cddb82132e3fb053
Md.Rafsanjani <rafsan.rifat255@gmail.com>
2021-05-25 15:51:50 +0600

    show file added

--- django-env/bookstore/books/urls.py
+++ django-env/bookstore/books/urls.py
@@ -2,8 +2,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.index)  # eikhane /book path a jokhon kono request ashbe, tokhon eita views file er index function
-                                # k load korbe, r tarpor response korbe
-
+    path('', views.index),  # eikhane /book path a jokhon kono request ashbe, tokhon eita views file er index function
+                                # k load korbe, r tarpor response korbe.
+    path('<int:id>', views.show)
 
 ]
