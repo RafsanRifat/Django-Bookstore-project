1452a2034bf9e3ad479a68b4d492f711b9423c74
Md.Rafsanjani <rafsan.rifat255@gmail.com>
2021-05-29 12:06:12 +0600

    new fields added to the book table of my database

--- django-env/bookstore/books/models.py
+++ django-env/bookstore/books/models.py
@@ -3,4 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Book(models.Model):
-    title = models.CharField(max_length=256)
+    title = models.CharField(max_length=256, null=True)
+    pageCount = models.IntegerField(default= 0)
+    thumbnailUrl = models.CharField(max_length=256, null=True)
+    shortDescription = models.CharField(max_length=256, null=True)
+    longDescription = models.TextField(null=True)
