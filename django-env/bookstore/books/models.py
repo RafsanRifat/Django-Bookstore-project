bbb7a7b62d62f5f1c2080fdc7d7151969a08f698
Md.Rafsanjani <rafsan.rifat255@gmail.com>
2021-06-10 11:28:32 +0600

    new model added

--- django-env/bookstore/books/models.py
+++ django-env/bookstore/books/models.py
@@ -2,12 +2,19 @@ from django.db import models
 
 # Create your models here.
 
+class Authors(models.Model):
+    name = models.CharField(max_length=256)
+    created_at = models.DateTimeField(auto_now=True)
+
+
+
 class Book(models.Model):
     title = models.CharField(max_length=256)
     pageCount = models.IntegerField(default= 0)
     thumbnailUrl = models.CharField(max_length=256, null=True)
     shortDescription = models.CharField(max_length=256, null=True)
     longDescription = models.TextField(null=True)
+    authors = models.ManyToManyField(Authors)
 
     def __str__(self):
         return self.title
@@ -19,3 +26,6 @@ class Review(models.Model):
     created_at = models.DateTimeField(auto_now=True)
     # book_id = models.BigIntegerField(default=1)
     book = models.ForeignKey(Book, on_delete=models.CASCADE, null=True)
+
+
+
