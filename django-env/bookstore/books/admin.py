97de8e222017d519a51b9ec5e3c67109e454461d
Md.Rafsanjani <rafsan.rifat255@gmail.com>
2021-06-10 11:41:59 +0600

    Authors resistered in admin.py

--- django-env/bookstore/books/admin.py
+++ django-env/bookstore/books/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from books.models import Book
+from books.models import Book,Authors
 
 
 admin.site.register(Book)
+admin.site.register(Authors)
